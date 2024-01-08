c8fbdde387a3e1709e37d6ad4f6909ec8a28179f
bijay <khanalbju@gamil.com>
2024-01-08 16:51:51 +0545

    ran some test for recipe and recipe_ingredients

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models import Recipe, RecipeIngridents
@@ -7,6 +8,10 @@ from .models import Recipe, RecipeIngridents
 User = get_user_model()
 
 admin.site.register(Recipe)
+class RecipeInline(admin.StackedInline):
+    model = Recipe
+    extra = 0
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display =['id', 'name','user']
     readonly_fields = ['created', 'updated']
--- cafe/tests.py
+++ cafe/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Recipe
+from .models import RecipeIngridents
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+
+def test_user_pw(self):
+    checked= self.user_a.check_password('Django@123')
+    self.assertTrue(checked)
+
+class RecipeTestCase(TestCase):
+    def setup(self):
+        self.user_a = get_user_model().objects.create_user(username='Admin', password='Django@123')
+        self.recipe_a = Recipe.objects.create(
+            name='Pizza', 
+            user=self.user_a,
+ )
+        self.recipe_b = Recipe.objects.create(
+            name='Burger', 
+            user=self.user_a,
+        )
+        self.recipe_ingredients_a = RecipeIngridents.objects.create(
+            name='Chicken', 
+            quantity = 1,
+        )
+
+    # def test_user_count(self):
+    #     qs = User.objects.all()
+    #     self.assertEqual(qs.count(), 2) 
+
+    # def test_user_recipe_reverse_count(self):
+    #     user = self.user_a
+    #     qs = user.recipe_set.all()
+    #     self.assertEqual(qs.count(), 2) 
+    
+    def test_recipe_ingrediants_reverse_count(self):
+        recipe = self.recipe_a
+        qs = recipe.recipeingridents_set.all()
+        self.assertEqual(qs.count(), 1)
+
+
