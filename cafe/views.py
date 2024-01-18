74097b6ba464ccd92f895870e889639e0891ba64
bijay <khanalbju@gamil.com>
2024-01-18 16:53:18 +0545

    parent and child form

--- cafe/views.py
+++ cafe/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render , get_list_or_404 , get_object_or_404, redir
 from .models import Recipe, RecipeIngridents
 from django import forms
 from .forms import RecipeForm, RecipeIngredientForm
+from django.forms.models import modelformset_factory 
 
 # Create your views here.
 #CURD -> Create, Update, Retrieve, Delete
@@ -44,22 +45,35 @@ def recipe_create_view(request):## create -> list
 def recipe_update_view(request, id = None):
     obj = get_object_or_404(Recipe, id = id, user=request.user)
     form = RecipeForm(request.POST or None, instance = obj)
-    form_2 = RecipeIngredientForm(request.POST or None)
-    ingridents_forms = []
-    for ingridents_obj in obj.recipeingridents_set.all():
-        ingridents_forms.append(RecipeIngredientForm(request.POST or None))
-
+    #form_2 = RecipeIngredientForm(request.POST or None)
+    RecipeIngredientFormSet = modelformset_factory(RecipeIngridents, form=RecipeIngredientForm, extra=0)
+    #ingridents_forms = []
+    qs = obj.recipeingridents_set.all()
+    # for ingridents_obj in obj.recipeingridents_set.all():
+    #     ingridents_forms.append(RecipeIngredientForm(request.POST or None))
+    formset = RecipeIngredientFormSet(request.POST or None, queryset=qs)
     context={
         "form":form,
-        "form_2": form_2,
-        "ingridents_forms": ingridents_forms,
+        "formset":formset,# formset = ingridents_forms
         "object":obj
     }
-    if all([form.is_valid(), form_2.is_valid()]):
-        form.save(commit=False)
-        form_2.save(commit = False)
-        print =("form", form.cleaned_data)
-        context["message"] = "Recipe has been successfully updated!!!"
+    if all([form.is_valid(), formset.is_valid()]):
+        # form.save(commit=False)
+        # form_2.save(commit = False)
+        # print =("form", form.cleaned_data)
+        parent = form.save(commit=False)
+        parent.save()
+        for form in formset:
+            child = form.save(commit=False)
+            if child.recipe is None:
+                print("Added new")
+                child.recipe = parent
+            child.save()
+               
+            # child.recipe = parent
+            # child.save()
 
+    context["message"] = "Recipe has been successfully updated!!!"
     return render(request, "cafe/create-update.html", context)
+
 
